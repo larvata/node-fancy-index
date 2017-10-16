f95c176f125c6b0b7532d32fff26b24204b5fec8
Larvata <alzzlnewtype@gmail.com>
2017-10-16 10:41:32 +0800

    fix unexcept encode on download file

--- middlewares/serveFile.js
+++ middlewares/serveFile.js
@@ -1,12 +1,6 @@
-const path = require('path');
-
 const serveFile = async (options, req, res, next) => {
-  const { path: pth } = req;
-  const { basePath } = options;
-
-  // await send(ctx, pth, { root: basePath });
-  const fileFullPath = path.join(basePath, pth);
-  res.download(fileFullPath);
+  const { fullpath } = options;
+  res.download(fullpath);
 };
 
 module.exports = serveFile;
